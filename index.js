c455a2ef4ca31eefcba6800be84bd1353eeebdd1
morronepablo <morronepablo@gmail.com>
2021-11-10 12:46:02 -0300

    Correcto manejo de métodos Asincrónicos parte 1

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ $(document).ready(function() {
     }
     // Fin funcion formato moneda
     var funcion;
-    verificar_sesion();
-    llenar_productos();
+    setTimeout(verificar_sesion,2000);
+    //verificar_sesion();
+    
 
     async function read_notificaciones() {
         funcion = "read_notificaciones";
@@ -26,26 +27,29 @@ $(document).ready(function() {
                 let notificaciones = JSON.parse(response);
                 console.log(notificaciones);
                 let template1 = '';
-                let template2 = '';
-                if(notificaciones.length == 0) {
-                    template1 += `
-                        <i class="far fa-bell"></i>
-                    `;
-                    template2 += `
-                        Notificaciones
-                    `;
-                } else {
-                    template1 += `
-                        <i class="far fa-bell"></i>
-                        <span class="badge badge-warning navbar-badge">${notificaciones.length}</span>
-                    `;
-                    template2 += `
-                        Notificaciones <span class="badge badge-warning right">${notificaciones.length}</span>
-                    `;
-                }
-                $('#numero_notificacion').html(template1);
-                $('#nav_cont_noti').html(template2);
-                let template = '';
+                let template = `
+                <a class="nav-link" data-toggle="dropdown" href="#">`;
+                    if(notificaciones.length == 0) {
+                        template += `
+                            <i class="far fa-bell"></i>
+                        `;
+                        template1 += `
+                            Notificaciones
+                        `;
+                    } else {
+                        template += `
+                            <i class="far fa-bell"></i>
+                            <span class="badge badge-warning navbar-badge">${notificaciones.length}</span>
+                        `;
+                        template1 += `
+                            Notificaciones <span class="badge badge-warning right">${notificaciones.length}</span>
+                        `;
+                    }
+                template += `</a>
+                <div id="notificaciones" class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                `;
+                $('#nav_cont_noti').html(template1);
+                
                 template += `
                     <span class="dropdown-item dropdown-header">${notificaciones.length} Notificaciones</span>
                 `;
@@ -78,8 +82,8 @@ $(document).ready(function() {
                 });
                 template += `
                 <a href="Views/notificaciones.php" class="dropdown-item dropdown-footer">Ver todas las notificaciones</a>
-                `;
-                $('#notificaciones').html(template);
+                </div>`;
+                $('#notificacion').html(template);
             } catch (error) {
                 console.error(error);
                 console.log(response);
@@ -180,32 +184,160 @@ $(document).ready(function() {
         }
     }
 
-    function verificar_sesion() {
-        funcion = 'verificar_sesion';
-        $.post('Controllers/UsuarioController.php', { funcion }, (response) => {
-            if(response != '') {
-                let sesion = JSON.parse(response);
-                $('#nav_login').hide();
-                $('#nav_register').hide();
-                $('#usuario_nav').text(sesion.user + ' #' + sesion.id);
-                console.log(sesion.avatar);
-                $('#avatar_nav').attr('src', 'Util/Img/Users/' + sesion.avatar);
-                $('#avatar_menu').attr('src', 'Util/Img/Users/' + sesion.avatar);
-                $('#usuario_menu').text(sesion.user);
-                read_notificaciones();
-                $('#notificacion').show();
-                $('#nav_notificaciones').show();
-                read_favoritos();
-                $('#favorito').show();
-                $('#nav_favoritos').show();
-            } else {
-                $('#nav_usuario').hide();
-                $('#notificacion').hide();
-                $('#nav_notificaciones').hide();
-                $('#favorito').hide();
-                $('#nav_favoritos').hide();
-            }
+    function llenar_menu_superior(usuario) {
+        let template = ``;
+        if(usuario === undefined || usuario == '' || usuario == null) {
+            template = `
+            <li class="nav-item">
+              <a class="nav-link" href="Views/register.php" role="button">
+                <i class="fas fa-user-plus"></i> Registrarse
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Views/login.php" role="button">
+                <i class="far fa-user"></i> Iniciar sesión
+              </a>
+            </li>
+            `;
+        } else {
+            template = `
+            <li class="nav-item dropdown">
+              <a class="nav-link" data-toggle="dropdown" href="#">
+                <i class="fas fa-shopping-cart"></i>
+                <span class="badge badge-danger navbar-badge">3</span>
+              </a>
+              <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                <a href="#" class="dropdown-item">
+                  <div class="media">
+                    <img src="../../dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+                    <div class="media-body">
+                      <h3 class="dropdown-item-title">
+                        Brad Diesel
+                        <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+                      </h3>
+                      <p class="text-sm">Call me whenever you can...</p>
+                      <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                    </div>
+                  </div>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item">
+                  <div class="media">
+                    <img src="../../dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                    <div class="media-body">
+                      <h3 class="dropdown-item-title">
+                        John Pierce
+                        <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
+                      </h3>
+                      <p class="text-sm">I got your message bro</p>
+                      <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                    </div>
+                  </div>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item">
+                  <div class="media">
+                    <img src="../../dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                    <div class="media-body">
+                      <h3 class="dropdown-item-title">
+                        Nora Silvester
+                        <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
+                      </h3>
+                      <p class="text-sm">The subject goes here</p>
+                      <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                    </div>
+                  </div>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+              </div>
+            </li>
+            <li id="notificacion" class="nav-item dropdown">
+              
+            </li>
+            <li id="favorito" class="nav-item dropdown">
+              <a id="numero_favorito" class="nav-link" data-toggle="dropdown" href="#">
+                <i class="far fa-heart"></i>
+                <span class="badge badge-warning navbar-badge">15</span>
+              </a>
+              <div id="favoritos" class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                <span class="dropdown-item dropdown-header">15 Notifications</span>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item">
+                  <i class="fas fa-envelope mr-2"></i> 4 new messages
+                  <span class="float-right text-muted text-sm">3 mins</span>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item">
+                  <i class="fas fa-users mr-2"></i> 8 friend requests
+                  <span class="float-right text-muted text-sm">12 hours</span>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item">
+                  <i class="fas fa-file mr-2"></i> 3 new reports
+                  <span class="float-right text-muted text-sm">2 days</span>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <img  src="Util/Img/Users/${usuario.avatar}" width="30" height="30" class="img-fluid img-circle" alt="">
+                <span>${usuario.user}</span>
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li><a class="dropdown-item" href="Views/mi_perfil.php"><i class="fas fa-user-cog"></i> Mi perfil</a></li>
+                <li><a class="dropdown-item" href="#"><i class="fas fa-shopping-basket"></i> Mi pedidos</a></li>
+                <li><a class="dropdown-item" href="Controllers/logout.php"><i class="fas fa-user-times"></i> Cerrar sesión</a></li>
+              </ul>
+            </li>
+            `;
+        }
+        $('#loader_1').hide(500);
+        $('#menu_superior').html(template);
+    }
+
+    async function verificar_sesion() {
+        funcion = "verificar_sesion";
+        let data = await fetch('Controllers/UsuarioController.php',{
+            method: 'POST',
+            headers: {'Content-Type':'application/x-www-form-urlencoded'},
+            body: 'funcion='+funcion
         })
+        if(data.ok) {
+            let response = await data.text();
+            //conselo.log(response);
+            try {
+                //let productos = JSON.parse(response);
+                //console.log(productos);
+                if(response != '') {
+                    let sesion = JSON.parse(response);
+                    llenar_menu_superior(sesion);
+                    $('#avatar_menu').attr('src', 'Util/Img/Users/' + sesion.avatar);
+                    $('#usuario_menu').text(sesion.user);
+                    read_notificaciones();
+                    read_favoritos();
+                    $('#favorito').show();
+                    $('#nav_favoritos').show();
+                } else {
+                    llenar_menu_superior();
+                    $('#favorito').hide();
+                    $('#nav_favoritos').hide();
+                }
+                llenar_productos();
+
+            } catch (error) {
+                console.error(error);
+                console.log(response);
+            }
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: data.statusText,
+                text: 'Hubo un conflicto de código: ' + data.status,
+            })
+        }
     }
     async function llenar_productos() {
         funcion = "llenar_productos";
